9b0b6c64f742f5cfb62b7c40fe16adca15d43d06
jarp78 <jarp78@gmail.com>
2020-09-25 21:01:15 -0500

    added addItem and decreaseItem functionality to checkout-item component

--- src/redux/cart/cart.utils.js
+++ src/redux/cart/cart.utils.js
@@ -11,5 +11,27 @@ export const addItemToCart = (cartItems, cartItemToAdd) => {
     );
   }
 
-  return [...cartItems, { ...cartItemToAdd, quantity: 1 }]
+  return [...cartItems, { ...cartItemToAdd, quantity: 1 }];
+};
+
+export const removeItemFromCart = (cartItems, cartItemToRemove) => {
+  return cartItems.filter((cartItem) => cartItem.id !== cartItemToRemove.id);
+};
+
+export const decreaseItemQuantity = (cartItems, cartItemToDecreaseQuantity) => {
+  const existingCartItem = cartItems.find(
+    (cartItem) => cartItem.id === cartItemToDecreaseQuantity.id
+  );
+
+  // if it is last item quantity, remove item from cart
+  if (existingCartItem.quantity === 1) {
+    return removeItemFromCart(cartItems, cartItemToDecreaseQuantity);
+  }
+
+  // there are more than one quantity, so, just decrease it by one
+  return cartItems.map((cartItem) =>
+    cartItem.id === cartItemToDecreaseQuantity.id
+      ? { ...cartItem, quantity: cartItem.quantity - 1 }
+      : cartItem
+  );
 };
